345e208c1a7b224602d654f9e144c6816893cc8a
Ivaylo1992 <ivaylop123@gmail.com>
2023-09-08 22:19:36 +0300

    random password generator

---
+++ password_generator.py
@@ -0,0 +1,38 @@
+
+import string
+import random
+
+characters = list(string.ascii_letters + string.digits + '!@#$%^&*()')
+
+
+#generate a password
+def generate_password():
+    # If yes, ask for length
+
+    password_length = int(input('How long would you like your password to be? '))
+
+    random.shuffle(characters)
+
+    password = []
+
+    for x in range(password_length):
+        password.append(random.choice(characters))
+
+    random.shuffle(password)
+
+    password = ''.join(password)
+    print(password)
+
+#Ask the user if they want to generate a password
+option = input('Do you like to generate a password? (Yes/No): ')
+
+#Print the passowrd
+if option == 'Yes':
+    generate_password()
+    # if the initial answer is no , exit the program
+elif option == 'No':
+    print('Program ended')
+    quit()
+else:
+    print('Invalid input!')
+    quit()
