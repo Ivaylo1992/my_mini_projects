2eff3bb58979a008c853a9f70f7a1340412ad181
Ivaylo1992 <ivaylop123@gmail.com>
2023-09-13 06:05:22 +0300

    binary search engine

---
+++ binary_search.py
@@ -0,0 +1,31 @@
+# a function that takes a list and a target parameter
+# multiple variables: middle, start, end and step
+# recursion or while loop
+# increase the steps each time a split is done
+# conditions to track target position
+
+def binary_search(lst, element):
+    middle = 0
+    start = 0
+    end = len(lst)
+    steps = 0
+
+    while start <= end:
+        print(f'Step {steps + 1} : {str(lst[start:end + 1])}')
+
+        steps += 1
+        middle = (start + end) // 2
+
+        if element == lst[middle]:
+            return middle
+        if element < lst[middle]:
+            end = middle - 1
+        else:
+            start = middle + 1
+
+    return - 1
+
+
+my_list = list(map(int, input('Enter your list of numbers, each separated by a single space: ').split()))
+target = int(input('Enter your target number: '))
+binary_search(my_list, target)
